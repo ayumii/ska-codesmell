85cbc35d36604620667412e8916d08e0ac0e5ebb
angela <angela.heng99@titansoft.com.sg>
2016-08-04 15:27:11 +0800

    Used TestCaseSource instead of TestCase for unit test

--- GeneralizationCsUnitTest/AddEmployeeCmdTest.cs
+++ GeneralizationCsUnitTest/AddEmployeeCmdTest.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Linq;
 using GeneralizationCs;
 using NUnit.Framework;
 
@@ -8,61 +9,7 @@ namespace GeneralizationCsUnitTest
 	[TestFixture]
     public class AddEmployeeCmdTest
 	{
-		[TestCase(0)]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(4)]
-        [TestCase(5)]
-        [TestCase(6)]
-        [TestCase(7)]
-        [TestCase(8)]
-        [TestCase(9)]
-        [TestCase(10)]
-        [TestCase(11)]
-        [TestCase(12)]
-        [TestCase(13)]
-        [TestCase(14)]
-        [TestCase(15)]
-        [TestCase(16)]
-        [TestCase(17)]
-        [TestCase(18)]
-        [TestCase(19)]
-        [TestCase(20)]
-        [TestCase(21)]
-        [TestCase(22)]
-        [TestCase(23)]
-        [TestCase(24)]
-        [TestCase(25)]
-        [TestCase(26)]
-        [TestCase(27)]
-        [TestCase(28)]
-        [TestCase(29)]
-        [TestCase(30)]
-        [TestCase(31)]
-        [TestCase(32)]
-        [TestCase(33)]
-        [TestCase(34)]
-        [TestCase(35)]
-        [TestCase(36)]
-        [TestCase(37)]
-        [TestCase(38)]
-        [TestCase(39)]
-        [TestCase(40)]
-        [TestCase(41)]
-        [TestCase(42)]
-        [TestCase(43)]
-        [TestCase(44)]
-        [TestCase(45)]
-        [TestCase(46)]
-        [TestCase(47)]
-        [TestCase(48)]
-        [TestCase(49)]
-        [TestCase(50)]
-        [TestCase(51)]
-        [TestCase(52)]
-        public void SentCorrectly(int index) {
-            char[] knownGood = {(char)Constants.Hex0Xde, (char)Constants.Hex0Xad,
+        private static char[] _KnownGood = {(char)Constants.Hex0Xde, (char)Constants.Hex0Xad,
                                 (char)Constants.Dec53, (char)Constants.Dec50,
                                 (char)Constants.Hex0X02,
                                 'F', 'r', 'e', 'd', ' ', 'B', 'r', 'o', 'o', 'k', 's', (char)Constants.Hex0X00,
@@ -70,13 +17,18 @@ namespace GeneralizationCsUnitTest
                                 'S', 'p', 'r', 'i', 'n', 'g', 'f', 'i', 'e', 'l', 'd', (char)Constants.Hex0X00,
                                 'I', 'L', (char)Constants.Hex0X00,
                                 '7', '2', '0', '0', '0', (char)Constants.Hex0X00,
-                                (char)Constants.Hex0Xbe, (char)Constants.Hex0Xef}; 
+                                (char)Constants.Hex0Xbe, (char)Constants.Hex0Xef};
 
+	    private static int _Length = _KnownGood.Length;
+	    private int[] _TestCasesIndexes = Enumerable.Range(0, _Length-1).ToArray();
+        
+        [Test, TestCaseSource("_TestCasesIndexes")]
+        public void SentCorrectly(int index) {
             EmployeeDetail employeeDetail = new EmployeeDetail("Fred Brooks", "123 My House", "Springfield", "IL", "72000");
 			AddEmployeeCmd cmd = new AddEmployeeCmd(employeeDetail);
 			StringWriter writer = new StringWriter();
 			cmd.Write(writer);
-            Assert.AreEqual(knownGood[index], writer.ToString()[index], "comparison failed at byte number " + index);
+            Assert.AreEqual(_KnownGood[index], writer.ToString()[index], "comparison failed at byte number " + index);
         }
 	}
 }
--- GeneralizationCsUnitTest/LoginCommandTest.cs
+++ GeneralizationCsUnitTest/LoginCommandTest.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Linq;
 using GeneralizationCs;
 using NUnit.Framework;
 
@@ -7,40 +8,23 @@ namespace GeneralizationCsUnitTest
 
 	[TestFixture] public class LoginCommandTest 
 	{
-		[TestCase(0)]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(4)]
-        [TestCase(5)]
-        [TestCase(6)]
-        [TestCase(7)]
-        [TestCase(8)]
-        [TestCase(9)]
-        [TestCase(10)]
-        [TestCase(11)]
-        [TestCase(12)]
-        [TestCase(13)]
-        [TestCase(14)]
-        [TestCase(15)]
-        [TestCase(16)]
-        [TestCase(17)]
-        [TestCase(18)]
-        [TestCase(19)]
-        [TestCase(20)]
-        public void SentCorrectly(int index) {                   
-            char[] knownGood = {(char)Constants.Hex0Xde, (char)Constants.Hex0Xad,
+        private static char[] _KnownGood = {(char)Constants.Hex0Xde, (char)Constants.Hex0Xad,
                                     (char)Constants.Dec50,(char)Constants.Dec48,
                                     (char)Constants.Hex0X01,
                                     'b', 'a', 'b', (char)Constants.Hex0X00,
                                     'c', 'a', 'r', 'd', 'i', 'n', 'a', 'l', 's', (char)Constants.Hex0X00,
                                     (char)Constants.Hex0Xbe, (char)Constants.Hex0Xef};
 
+        private static int _Length = _KnownGood.Length;
+        private int[] _TestCasesIndexes = Enumerable.Range(0, _Length-1).ToArray();
+
+        [Test,TestCaseSource("_TestCasesIndexes")]
+        public void SentCorrectly(int index) {                   
             LoginDetail loginDetail = new LoginDetail("bab", "cardinals");
             LoginCommand cmd = new LoginCommand(loginDetail);
 			StringWriter writer = new StringWriter();
 			cmd.Write(writer);
-            Assert.AreEqual(knownGood[index], writer.ToString()[index], "comparison failed at byte number " + index);
+            Assert.AreEqual(_KnownGood[index], writer.ToString()[index], "comparison failed at byte number " + index);
 		}
 	}
 }
